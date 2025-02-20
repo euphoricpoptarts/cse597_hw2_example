eac0604e5500d044d76f0eb84a057fa956c08245
Mike Gilbert <euphoricpoptarts@gmail.com>
2025-02-20 15:19:42 -0500

    use double instead of float; openmp; standalone driver for pagerank

--- pagerank.cpp
+++ pagerank.cpp
@@ -1,25 +1,28 @@
 #include <vector>
 #include <cmath>
 #include "csr_graph.h"
+#include "io.cpp"
 
-std::vector<float> normalize_columns(const csr_graph& g){
+std::vector<double> normalize_columns(const csr_graph& g){
     std::vector<int> degrees(g.t_vtx, 0);
     // since the graph is undirected, the row size is equal to the corresponding column size
     for(int v = 0; v < g.t_vtx; v++){
         degrees[v] = g.row_map[v+1] - g.row_map[v];
     }
-    std::vector<float> vals(g.nnz);
+    std::vector<double> vals(g.nnz);
     for(int v = 0; v < g.t_vtx; v++){
         for(int j = g.row_map[v]; j < g.row_map[v+1]; j++){
             int u = g.entries[j];
-            vals[j] = 1.0 / static_cast<float>(degrees[u]);
+            vals[j] = 1.0 / static_cast<double>(degrees[u]);
         }
     }
+    return vals;
 }
 
 // sparse matrix-vector multiply
-std::vector<float> spmv(const csr_graph& g, const std::vector<float>& vals, const std::vector<float>& input){
-    std::vector<float> output(g.t_vtx, 0);
+std::vector<double> spmv(const csr_graph& g, const std::vector<double>& vals, const std::vector<double>& input){
+    std::vector<double> output(g.t_vtx, 0);
+    #pragma omp parallel for
     for(int v = 0; v < g.t_vtx; v++){
         for(int j = g.row_map[v]; j < g.row_map[v+1]; j++){
             int u = g.entries[j];
@@ -29,31 +32,43 @@ std::vector<float> spmv(const csr_graph& g, const std::vector<float>& vals, cons
     return output;
 }
 
-void axpy(std::vector<float>& x, const float a, const float y){
+void axpy(std::vector<double>& x, const double a, const double y){
     for(int i = 0; i < x.size(); i++){
         x[i] = a*x[i] + y;
     }
 }
 
-float norm(const std::vector<float>& a, const std::vector<float>& b){
-    float x = 0;
+double norm(const std::vector<double>& a, const std::vector<double>& b){
+    double x = 0;
     for(int i = 0; i < a.size(); i++){
-        float diff = std::abs(a[i] - b[i]);
+        double diff = std::abs(a[i] - b[i]);
         x += diff*diff;
     }
     return std::sqrt(x);
 }
 
-std::vector<float> pagerank(const csr_graph& g, const float d, const float tol){
-    float damp_add = (1.0 - d) / static_cast<float>(g.t_vtx);
-    std::vector<float> transition_probs = normalize_columns(g);
-    std::vector<float> pr(g.t_vtx, 1.0 / static_cast<float>(g.t_vtx));
-    float diff = 100.0;
+std::vector<double> pagerank(const csr_graph& g, const double d, const double tol){
+    double damp_add = (1.0 - d) / static_cast<double>(g.t_vtx);
+    std::vector<double> transition_probs = normalize_columns(g);
+    std::vector<double> pr(g.t_vtx, 1.0 / static_cast<double>(g.t_vtx));
+    double diff = 100.0;
     while(diff > tol){
-        std::vector<float> pr_next = spmv(g, transition_probs, pr);
+        std::vector<double> pr_next = spmv(g, transition_probs, pr);
         axpy(pr_next, d, damp_add);
         diff = norm(pr_next, pr);
+        std::cout << "Normalized diff: " << diff << std::endl;
         pr = pr_next;
     }
     return pr;
+}
+
+int main(int argc, char** argv){
+    if(argc < 2){
+        std::cerr << "Too few arguments provided" << std::endl;
+        return -1;
+    }
+    csr_graph g = load_metis_graph(argv[1]);
+    if(g.error) return -1;
+    std::vector<double> rank = pagerank(g, 0.85, 0.000000001);
+    return 0;
 }
